2b920fb619c54036c4f8071c5e941a75f57a2992
hashtagobi1 <anokwuruobi@gmail.com>
2024-09-12 18:46:42 +0100

    added gTag script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { VT323 } from "next/font/google";
 import { Analytics } from "@vercel/analytics/react";
+import Script from "next/script";
 
 // Define the VT323 font imported from Google Fonts
 const VT_Font = VT323({
@@ -30,9 +31,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <Script
+        id="gtm"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-NWTMDQMD');`,
+        }}
+      ></Script>
+
       <body className={`${VT_Font.variable} ${nokiaFont.variable} antialiased`}>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-NWTMDQMD"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
         {children}
-        <Analytics/>
+        <Analytics />
       </body>
     </html>
   );
