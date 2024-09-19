d8238c971e8eafa88ecab6ffe42526b1fa296642
hashtagobi1 <anokwuruobi@gmail.com>
2024-09-19 18:43:39 +0100

    update success msg

--- src/components/SuccessMsg.tsx
+++ src/components/SuccessMsg.tsx
@@ -1,13 +1,12 @@
 import { CheckCircle2 } from "lucide-react";
-import { Alert, AlertDescription, AlertTitle } from "./ui/alert";
+import { Alert, AlertDescription } from "./ui/alert";
 
 export default function SuccessMsg() {
   return (
     <Alert>
       <CheckCircle2 className="h-4 w-4" />
-      <AlertTitle>Success</AlertTitle>
       <AlertDescription>
-        {"Thank you for signing up! We'll keep you updated on the release."}
+        {"Thank you for signing up! We'll let you know when it is out."}
       </AlertDescription>
     </Alert>
   );
