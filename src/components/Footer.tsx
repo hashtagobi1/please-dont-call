ac87459a307d67dbbd9c4457773a6e8cfd407ba8
hashtagobi1 <anokwuruobi@gmail.com>
2024-11-04 18:40:03 +0000

    added lightwave, changed spacing

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <footer className="mt-5 flex flex-col gap-2 text-xs ">
+    <footer className=" flex flex-col gap-2 text-xs ">
       <div className="flex justify-center gap-7  flex-row items-center">
         <a href="http://www.instagram.com/hashtagobi">
           <InstagramLogoIcon className="size-8" />
@@ -14,6 +14,7 @@ const Footer = () => {
         </a>
       </div>
       <p className="mt-2">© {new Date().getFullYear()} obi. All rights reserved.</p>
+      <p className="mt-2 text-center">made by LightWave</p>
     </footer>
   );
 };
